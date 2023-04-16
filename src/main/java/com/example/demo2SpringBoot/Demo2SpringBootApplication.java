4d395e5d9c1de163bfd1a7c2144714e9e609110b
SUHA <suha@zealand.dk>
2023-04-16 19:05:22 +0200

    Initial commit

---
+++ src/main/java/com/example/demo2SpringBoot/Demo2SpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo2SpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo2SpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demo2SpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demo2SpringBoot/Demo2SpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo2SpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo2SpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
